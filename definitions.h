57211758912e2f71093ecfdec3353350e3956e23
Ben Humphrey <44178664+cxmplex@users.noreply.github.com>
2021-05-11 01:03:41 -0400

    Create definitions.h

---
+++ definitions.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <ntifs.h> 
+#include <ntddk.h> 
+#include <ntstrsafe.h> 
+#include <stdlib.h>
+
+DWORD PEBLDR_OFFSET = 0x18; // peb.ldr
+DWORD PEBLDR_MEMORYLOADED_OFFSET = 0x10; // peb.ldr.InMemoryOrderModuleList
+
+typedef struct _LDR_DATA_TABLE_ENTRY
+{
+	LIST_ENTRY64	InLoadOrderLinks;
+	LIST_ENTRY64	InMemoryOrderLinks;
+	LIST_ENTRY64	InInitializationOrderLinks;
+	PVOID			DllBase;
+	UINT64			EntryPoint;
+	ULONG			SizeOfImage;
+	UNICODE_STRING	FullDllName;
+	UNICODE_STRING 	BaseDllName;
+	ULONG			Flags;
+	USHORT			LoadCount;
+	USHORT			TlsIndex;
+	PVOID			SectionPointer;
+	ULONG			CheckSum;
+	PVOID			LoadedImports;
+	PVOID			EntryPointActivationContext;
+	PVOID			PatchInformation;
+	LIST_ENTRY64	ForwarderLinks;
+	LIST_ENTRY64	ServiceTagLinks;
+	LIST_ENTRY64	StaticLinks;
+	PVOID			ContextInformation;
+	ULONG64			OriginalBase;
+	LARGE_INTEGER	LoadTime;
+} LDR_DATA_TABLE_ENTRY, * PLDR_DATA_TABLE_ENTRY;
+
+
+DECLSPEC_IMPORT PPEB NTAPI PsGetProcessPeb(
+	IN PEPROCESS Process
+);
+
+NTSTATUS NTAPI MmCopyVirtualMemory
+(
+	PEPROCESS SourceProcess,
+	PVOID SourceAddress,
+	PEPROCESS TargetProcess,
+	PVOID TargetAddress,
+	SIZE_T BufferSize,
+	KPROCESSOR_MODE PreviousMode,
+	PSIZE_T ReturnSize
+);
+
+DECLSPEC_IMPORT PPEB NTAPI PsGetProcessPeb(
+	IN PEPROCESS Process
+);
+
+NTSTATUS NTAPI MmCopyVirtualMemory
+(
+	PEPROCESS SourceProcess,
+	PVOID SourceAddress,
+	PEPROCESS TargetProcess,
+	PVOID TargetAddress,
+	SIZE_T BufferSize,
+	KPROCESSOR_MODE PreviousMode,
+	PSIZE_T ReturnSize
+);
+
+NTKERNELAPI PVOID PsGetProcessSectionBaseAddress(
+	__in PEPROCESS Process
+);
